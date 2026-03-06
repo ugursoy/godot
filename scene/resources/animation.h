759f3655771ee22f97a5c94223ff5491a7dae46b
M. Sadık Uğursoy <sadikugursoy@gmail.com>
2026-03-06 04:14:29 +0300

    Use Pair<NodePath, TrackPath> as track_cache key

--- scene/resources/animation.h
+++ scene/resources/animation.h
@@ -40,8 +40,6 @@ class Animation : public Resource {
 	RES_BASE_EXTENSION("anim");
 
 public:
-	typedef uint32_t TypeHash;
-
 	static inline String PARAMETERS_BASE_PATH = "parameters/";
 	static constexpr real_t DEFAULT_STEP = 1.0 / 30;
 
@@ -104,12 +102,13 @@ public:
 	};
 #endif // TOOLS_ENABLED
 
+	typedef Pair<NodePath, Animation::TrackType> TrackCacheId;
 	struct Track {
 		TrackType type = TrackType::TYPE_ANIMATION;
 		InterpolationType interpolation = INTERPOLATION_LINEAR;
 		bool loop_wrap = true;
 		NodePath path; // Path to something.
-		TypeHash thash = 0; // Hash by Path + SubPath + TrackType.
+		TrackCacheId cache_id;
 		bool imported = false;
 		bool enabled = true;
 		virtual ~Track() {}
@@ -288,8 +287,6 @@ private:
 	bool capture_included = false;
 	void _check_capture_included();
 
-	void _track_update_hash(int p_track);
-
 	/* Animation compression page format (version 1):
 	 *
 	 * Animation uses bitwidth based compression separated into small pages. The intention is that pages fit easily in the cache, so decoding is cache efficient.
@@ -424,7 +421,7 @@ public:
 	NodePath track_get_path(int p_track) const;
 	int find_track(const NodePath &p_path, const TrackType p_type) const;
 
-	TypeHash track_get_type_hash(int p_track) const;
+	TrackCacheId track_get_cache_id(int p_track) const;
 
 	void track_move_up(int p_track);
 	void track_move_down(int p_track);
